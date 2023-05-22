34341a7f9bb199a64822e9a4c191ad08f7e2dc25
Lord-Turmoil <68287135+Lord-Turmoil@users.noreply.github.com>
2023-05-22 10:23:37 +0000

    fix footer bug Updated By Github Actions

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,11 @@
 // custom title name
-var n = document.getElementById('site-title');
-n.innerHTML = "Tony's Studio";
+var t = document.getElementById('site-title');
+if (t != null) {
+    t.innerHTML = "Tony's Studio";
+}
+
 // custom style for author
-var c = document.getElementsByClassName("copyright")[0];
-c.innerHTML = c.innerHTML.replace('Tony Skywalker', '<span class="author">Tony Skywalker</span>');
+var c = document.getElementById("footer-wrap").getElementsByClassName("copyright")[0];
+if (c != null) {
+    c.innerHTML = c.innerHTML.replace('Tony Skywalker', '<span class="author">Tony Skywalker</span>');
+}
