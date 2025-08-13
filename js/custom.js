2e20c382fe9c96f1c6faeed813b0453703884c9b
Lord-Turmoil <swift-phantom@outlook.com>
2025-08-13 10:03:20 +0000

    fix: link and custom script Updated By Github Actions

--- js/custom.js
+++ js/custom.js
@@ -12,19 +12,24 @@ const TITLE_MAP = {
     "/": "Tony's Studio"
 };
 
-var t = document.getElementById('site-title');
-if (t != null) {
-    var router = window.location.pathname;
-    for (var key in TITLE_MAP) {
-        if (router.startsWith(key)) {
-            t.innerHTML = TITLE_MAP[key];
-            break;
+document.addEventListener("DOMContentLoaded", function () {
+    var siteTitle = document.getElementById('site-title');
+    if (siteTitle != null) {
+        var router = window.location.pathname;
+        for (var key in TITLE_MAP) {
+            if (router.startsWith(key)) {
+                siteTitle.innerHTML = TITLE_MAP[key];
+                break;
+            }
         }
     }
-}
 
-// custom style for author
-var c = document.getElementById("footer").getElementsByClassName("copyright")[0];
-if (c != null) {
-    c.innerHTML = c.innerHTML.replace('Tony Lewis', '<span class="author">Tony Lewis</span>');
-}
+    var footer = document.getElementById("footer");
+    var copyright = footer.getElementsByClassName("copyright")[0];
+    if (copyright != null) {
+        copyright.innerHTML = copyright.innerHTML.replace('Tony Lewis', '<span class="author">Tony Lewis</span>');
+    }
+    var badges = document.getElementById("ghbdages");   // bad spelling >:(
+    var footerOther = footer.getElementsByClassName("footer-other")[0];
+    footerOther.appendChild(badges);
+});
