a94bb1fd20036f5f8db2fba9678fef6d8a8117ab
Lord-Turmoil <swift-phantom@outlook.com>
2025-09-16 09:24:01 +0000

    update: use small cap for title Updated By Github Actions

--- static/custom.js
+++ static/custom.js
@@ -1,24 +1,35 @@
 // https://minify-js.com/
 
 const TITLE_MAP = {
-    "/archives": "Archives",
-    "/categories": "Categories",
-    "/tags": "Tags",
-    "/gallery": "Gallery",
-    "/link": "Links & Projects",
-    "/support": "Support Me",
-    "/about": "About Me",
-    "/404": "404 Not Found",
-    "/": "Tony's Studio"
+    "/archives": "ARCHIVE",
+    "/categories": "CATEGORIES",
+    "/tags": "TAGS",
+    "/gallery": "GALLERY",
+    "/link": "FRIENDS & LINKS",
+    "/support": "SUPPORT",
+    "/about": "ABOUT ME",
+    "/404": "404 NOT FOUND",
+    "/": "TONY's STUDIO"
 };
 
 document.addEventListener("DOMContentLoaded", function () {
+    function shouldCapitalize(char) {
+        return /[A-Z]/.test(char);
+    }
+
     var siteTitle = document.getElementById('site-title');
     if (siteTitle != null) {
         var router = window.location.pathname;
         for (var key in TITLE_MAP) {
             if (router.startsWith(key)) {
-                siteTitle.innerHTML = TITLE_MAP[key];
+                const title = TITLE_MAP[key];
+                siteTitle.innerHTML = title.replace(/\b\w/g, function (char) {
+                    // if first letter is capital and not a number, wrap it in a span
+                    if (shouldCapitalize(char)) {
+                        return '<span class="first-letter">' + char + '</span>';
+                    }
+                    return char;
+                });
                 break;
             }
         }
