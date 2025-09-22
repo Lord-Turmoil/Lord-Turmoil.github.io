52dd0b974c6091bd757bdff2f73bf2234860e41c
Lord-Turmoil <swift-phantom@outlook.com>
2025-09-22 02:09:32 +0000

    update: use small-caps style Updated By Github Actions

--- static/custom.js
+++ static/custom.js
@@ -1,35 +1,24 @@
 // https://minify-js.com/
 
 const TITLE_MAP = {
-    "/archives": "ARCHIVE",
-    "/categories": "CATEGORIES",
-    "/tags": "TAGS",
-    "/gallery": "GALLERY",
-    "/link": "FRIENDS & LINKS",
-    "/support": "SUPPORT",
-    "/about": "ABOUT ME",
-    "/404": "404 NOT FOUND",
-    "/": "TONY's STUDIO"
+    "/archives": "Archives",
+    "/categories": "Categories",
+    "/tags": "Tags",
+    "/gallery": "Gallery",
+    "/link": "Friends & Links",
+    "/support": "Support",
+    "/about": "About Me",
+    "/404": "404 Not Found",
+    "/": "Tony's Studio"
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-    function shouldCapitalize(char) {
-        return /[A-Z]/.test(char);
-    }
-
     var siteTitle = document.getElementById('site-title');
     if (siteTitle != null) {
         var router = window.location.pathname;
         for (var key in TITLE_MAP) {
             if (router.startsWith(key)) {
-                const title = TITLE_MAP[key];
-                siteTitle.innerHTML = title.replace(/\b\w/g, function (char) {
-                    // if first letter is capital and not a number, wrap it in a span
-                    if (shouldCapitalize(char)) {
-                        return '<span class="first-letter">' + char + '</span>';
-                    }
-                    return char;
-                });
+                siteTitle.innerHTML = TITLE_MAP[key]
                 break;
             }
         }
