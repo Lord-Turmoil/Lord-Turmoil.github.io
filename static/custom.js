578b3eaee3c40c8a4b078ca33281b8d6b974d0be
Lord-Turmoil <swift-phantom@outlook.com>
2025-09-29 05:31:57 +0000

    update: use minifier to reduce static resource size Updated By Github Actions

--- static/custom.js
+++ static/custom.js
@@ -1,18 +1,4 @@
-// https://minify-js.com/
-
-const TITLE_MAP = {
-    "/archives": "Archives",
-    "/categories": "Categories",
-    "/tags": "Tags",
-    "/gallery": "Gallery",
-    "/link": "Friends & Links",
-    "/support": "Support",
-    "/about": "About Me",
-    "/404": "404 Not Found",
-    "/": "Tony's Studio"
-};
-
-const ANNOUNCEMENT = `
+let TITLE_MAP={"/archives":"Archives","/categories":"Categories","/tags":"Tags","/gallery":"Gallery","/link":"Friends & Links","/support":"Support","/about":"About Me","/404":"404 Not Found","/":"Tony's Studio"},ANNOUNCEMENT=`
 <div>
 It won't be long before the armies of the Republic track us here.<br>
 You can visit this site at:
@@ -20,30 +6,4 @@ You can visit this site at:
 <li><a href="https://blog.zhengyaoliu.com/" target="_blank">Vercel Mirror Site</a></li>
 <li><a href="https://lord-turmoil.github.io/" target="_blank">Github Mirror Site</a></li>
 </div>
-`;
-
-document.addEventListener("DOMContentLoaded", function () {
-    var siteTitle = document.getElementById('site-title');
-    if (siteTitle != null) {
-        var router = window.location.pathname;
-        for (var key in TITLE_MAP) {
-            if (router.startsWith(key)) {
-                siteTitle.innerHTML = TITLE_MAP[key]
-                break;
-            }
-        }
-    }
-
-    var footer = document.getElementById("footer");
-    var copyright = footer.getElementsByClassName("copyright")[0];
-    if (copyright != null) {
-        copyright.innerHTML = copyright.innerHTML.replace('Tony Lewis', '<span class="author">Tony Lewis</span>');
-    }
-    var badges = document.getElementById("ghbdages");   // bad spelling >:(
-    var footerOther = footer.getElementsByClassName("footer-other")[0];
-    footerOther.appendChild(badges);
-
-    document.querySelectorAll('div.announcement_content').forEach(function (element) {
-        element.innerHTML = ANNOUNCEMENT;
-    });
-});
+`;document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("site-title");if(null!=e){var t,n=window.location.pathname;for(t in TITLE_MAP)if(n.startsWith(t)){e.innerHTML=TITLE_MAP[t];break}}var i=document.getElementById("footer"),o=i.getElementsByClassName("copyright")[0],o=(null!=o&&(o.innerHTML=o.innerHTML.replace("Tony Lewis",'<span class="author">Tony Lewis</span>')),document.getElementById("ghbdages"));i.getElementsByClassName("footer-other")[0].appendChild(o),document.querySelectorAll("div.announcement_content").forEach(function(e){e.innerHTML=ANNOUNCEMENT})});
